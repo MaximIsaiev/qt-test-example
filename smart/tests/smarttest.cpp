bdd458cdad54ab386d38f7ec7144ab245857f1c9
PaketHead <meddan4k2@gmail.com>
2025-06-19 17:25:28 +0300

    [+] Tests for class Smart.

--- smart/tests/smarttest.cpp
+++ smart/tests/smarttest.cpp
@@ -1,6 +1,5 @@
 #include <smart.h>
 #include "smarttest.h"
-
 void SmartTest::initTestCase()
 {
     qDebug("Called before everything else.");
@@ -18,6 +17,42 @@ void SmartTest::exampleTest()
     Smart smart;
     QVERIFY(!Smart::threeConsecutiveOdds(arr));
     QVERIFY(Smart::threeConsecutiveOdds(arr1));
+}
+
+void SmartTest::test1() {
+    std::vector<int> arr{3, 3, 3};
+    QVERIFY(Smart::threeConsecutiveOdds(arr));
+}
+
+
+void SmartTest::pushBackTest() {
+    std::vector<int> arr{3, 3};
+    QVERIFY(!Smart::threeConsecutiveOdds(arr));
+    arr.push_back(5);
+    QVERIFY(Smart::threeConsecutiveOdds(arr));
+}
+
+void SmartTest::highIntTest() {
+    std::vector<int> arr1{2147483647, 2147483647, 2147483647};
+    QVERIFY(Smart::threeConsecutiveOdds(arr1));
+}
+
+void SmartTest::negativeIntTest() {
+    std::vector<int> arr{1, 4, 5, -3, 7, 8};
+    qDebug("It should be true, right?");
+    QVERIFY(Smart::threeConsecutiveOdds(arr));
+}
+
+void SmartTest::bigArrayTest() {
+    std::vector<int> arr;
+    for(int i = 0; i < 100000000; i++) {
+        arr.push_back(i);
+        if(i == 9999999) {
+            arr.push_back(3);
+            arr.push_back(551);
+        }
+    }
+    QVERIFY(Smart::threeConsecutiveOdds(arr));
 }
 
 QTEST_APPLESS_MAIN(SmartTest)
--- smart/tests/smarttest.h
+++ smart/tests/smarttest.h
@@ -1,12 +1,15 @@
 #pragma once
 #include <QtTest/QTest>
-
 class SmartTest: public QObject
 {
     Q_OBJECT
-
 private slots:
     void initTestCase();
     void exampleTest();
     void cleanupTestCase();
+    void test1();
+    void pushBackTest();
+    void highIntTest();
+    void negativeIntTest();
+    void bigArrayTest();
 };
