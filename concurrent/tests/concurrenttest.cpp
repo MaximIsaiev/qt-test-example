d4b1951df8abe77caafa648acffed331e7508f98
PaketHead <meddan4k2@gmail.com>
2025-06-24 11:15:39 +0300

    [+] пофиксил невнимательность, пока что в плане кода((((

--- concurrent/tests/concurrenttest.cpp
+++ concurrent/tests/concurrenttest.cpp
@@ -45,9 +45,9 @@ void ConcurrentTest::sleepTest1()
     {
         result += "third";
     };
-    std::thread b(&Concurrent::first, &con, first);
+    std::thread b(&Concurrent::second, &con, second);
     std::this_thread::sleep_for(std::chrono::milliseconds(500));
-    std::thread c(&Concurrent::second, &con, second);
+    std::thread c(&Concurrent::first, &con, first);
     std::thread a(&Concurrent::third, &con, third);
     a.join();
     b.join();
@@ -72,11 +72,11 @@ void ConcurrentTest::sleepTest2()
     {
         result += "third";
     };
-    std::thread c(&Concurrent::first, &con, first);
+    std::thread c(&Concurrent::third, &con, third);
     std::this_thread::sleep_for(std::chrono::milliseconds(500));
     std::thread b(&Concurrent::second, &con, second);
     std::this_thread::sleep_for(std::chrono::milliseconds(1500));
-    std::thread a(&Concurrent::third, &con, third);
+    std::thread a(&Concurrent::first, &con, first);
     a.join();
     b.join();
     c.join();
@@ -102,9 +102,9 @@ void ConcurrentTest::sleepTest3()
     };
     std::thread c(&Concurrent::first, &con, first);
     std::this_thread::sleep_for(std::chrono::milliseconds(200));
-    std::thread b(&Concurrent::second, &con, second);
+    std::thread b(&Concurrent::third, &con, third);
     std::this_thread::sleep_for(std::chrono::milliseconds(300));
-    std::thread a(&Concurrent::third, &con, third);
+    std::thread a(&Concurrent::second, &con, second);
     std::this_thread::sleep_for(std::chrono::milliseconds(400));
     a.join();
     b.join();
@@ -129,11 +129,11 @@ void ConcurrentTest::sleepTest4()
     {
         result += "third";
     };
-    std::thread b(&Concurrent::first, &con, first);
+    std::thread b(&Concurrent::second, &con, second);
     std::this_thread::sleep_for(std::chrono::milliseconds(500));
-    std::thread a(&Concurrent::second, &con, second);
+    std::thread a(&Concurrent::third, &con, third);
     std::this_thread::sleep_for(std::chrono::milliseconds(800));
-    std::thread c(&Concurrent::third, &con, third);
+    std::thread c(&Concurrent::first, &con, first);
     a.join();
     b.join();
     c.join();
